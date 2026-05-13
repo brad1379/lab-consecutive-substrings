2270bf756ff751a867e725d947aec53b33a466d6
Brad Arnold <brad1379@gmail.com>
2026-05-13 12:28:14 -0400

    first commit

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,6 +1,20 @@
 function consecutiveSubstrings(string) {
-  // type your code here
+  const substringArray = []; // array to add the substrings
+  
+  for (let i = 0; i < string.length; i++) {
+    // Declare a temporary variable to assign 
+    // the characters of the string to
+    let temp = "" 
+    for (let j = i; j < string.length; j++) {
+      // add the current character to the temp 
+      // variable and push it to the array
+      temp = temp + string[j] 
+      substringArray.push(temp)
+    }
+  }
+  return substringArray;
 }
+
 
 if (require.main === module) {
   // add your own tests in here
